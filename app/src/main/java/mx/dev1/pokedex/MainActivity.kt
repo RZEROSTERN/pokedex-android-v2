47d8c5dad0613f4097e8e96f45dfb60a6a8239bd
Marco Ramirez <marco.ramirez@rzerocorp.com.mx>
2023-02-11 00:13:19 -0600

    Added Splash and updated Kotlin compiler and libraries

--- app/src/main/java/mx/dev1/pokedex/MainActivity.kt
+++ app/src/main/java/mx/dev1/pokedex/MainActivity.kt
@@ -4,40 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import mx.dev1.pokedex.ui.theme.NewPokedexTheme
+import mx.dev1.pokedex.ui.presentation.composables.Navigation
+import mx.dev1.pokedex.ui.theme.Red500
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewPokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                color = Red500,
+                modifier = Modifier.fillMaxSize()) {
+                Navigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NewPokedexTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/mx/dev1/pokedex/ui/theme/Color.kt
+++ app/src/main/java/mx/dev1/pokedex/ui/theme/Color.kt
@@ -2,7 +2,8 @@ package mx.dev1.pokedex.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
+val Red200 = Color(0xFFEE8596)
+val Red500 = Color(0xffDC0A2D)
+val Red700 = Color(0xFFD30722)
+
 val Teal200 = Color(0xFF03DAC5)
